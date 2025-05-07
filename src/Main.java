158ab5d47137c29fef9282472b688654cff871ee
Bion Ullenius <Bion.Holmberullenius@elev.ga.ntig.se>
2025-05-07 13:18:46 +0200

    färdig med att skriva av koden.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+/* Client */
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.*;
+import java.net.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Socket client = null;
+
+        //Default port number we are going to use
+        int portnumber = 55050;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+        for (int i = 0; i < 10; i++){
+            try {
+                String msg = "";
+
+                //Create a client socket
+                client = new Socket(InetAddress.getLocalHost(), portnumber);
+                System.out.println("Client socket is created " + client);
+
+                //Create an output stream of the client socket
+                OutputStream clientOut = client.getOutputStream();
+                PrintWriter pw = new PrintWriter(clientOut, true);
+
+                //Create an input stream of the client socket
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+
+                //Create BufferedReader for a standard input
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+                System.out.println("Enter your name. Type Bye to exit.");
+
+                //Read data from standard input device and write it to the output stream of the client socket.
+                msg = stdIn.readLine().trim();
+                pw.println(msg);
+
+                //Read data from the input stream of the client socket.
+                System.out.println("Message returned from the server = " + br.readLine());
+
+                pw.close();
+                br.close();
+                client.close();
+
+                //Stop the operation
+                if (msg.equalsIgnoreCase("Bye")){
+                    break;
+                }
+
+            } catch (IOException ie){
+                System.out.println("I/O error " + ie);
+            }
         }
     }
 }
